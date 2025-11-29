c3b6f8da2bc6eddfdae526fef34d4fb681508025
JambitX <2050890162@qq.com>
2025-11-29 23:27:45 +0800

    feat(scene): 添加启动动画StartupScene和村庄草图VillageScene
    - 新增 StartupScene 启动画面及进度条动画
    - 新增 VillageScene 村庄场景草图
    - 新增多张建筑与场景相关图片资源（如 StartupScene.png、VillageScene.png、loading_bar.png 及 buildings 目录下建筑图片）
    - 项目结构优化，部分文件移动至 Scene、AppDelegate 等子目录
    - 更新 .gitignore 和 CMakeLists.txt 以适配新资源与结构

---
+++ Classes/Scene/VillageScene.cpp
@@ -0,0 +1,36 @@
+﻿#include "VillageScene.h"
+
+USING_NS_CC;
+
+// ��������
+Scene* VillageScene::createScene() {
+  auto scene = Scene::create();
+  auto layer = VillageScene::create();
+  scene->addChild(layer);
+  return scene;
+}
+
+// ��ʼ��
+bool VillageScene::init() {
+  if (!Scene::init()) return false;
+
+  auto screenSize = Director::getInstance()->getVisibleSize();
+
+  // ��ׯ����
+  auto bg = Sprite::create("Scene/VillageScene.png");
+  bg->setPosition(screenSize / 2);
+  bg->Sprite::setScale(0.5);
+  this->addChild(bg);
+
+  //// �򵥽���
+  //auto townHall = Sprite::create("townhall.png");
+  //townHall->setPosition(screenSize.width / 2, screenSize.height / 2);
+  //this->addChild(townHall);
+
+
+  auto label = Label::createWithSystemFont("man! what can i say!", "Arial", 36);
+  label->setPosition(Vec2(screenSize.width / 2, screenSize.height - 80));
+  this->addChild(label);
+
+  return true;
+}
