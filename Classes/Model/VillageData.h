8d9f89e877dad71b5361c99267a1d8b628ec0a12
JambitX <2050890162@qq.com>
2025-12-06 13:59:15 +0800

    feat: VillageData 构建 modified:   CMakeLists.txt modified:   Classes/Layer/HUDLayer.cpp modified:   Classes/Layer/ShopLayer.cpp modified:   Classes/Layer/ShopLayer.h modified:   Classes/Manager/ResourceManager.cpp modified:   Classes/Model/VillageData.h modified:   Classes/Scene/VillageScene.cpp
    villagedata.h 头文件定义结构体类型(不需要cpp了)
    villagedatamanager.h/.cpp  创建资源单例对象

--- Classes/Model/VillageData.h
+++ Classes/Model/VillageData.h
@@ -8,7 +8,11 @@ struct BuildingInstance {
   int type;           // 建筑类型（如1=大本营，2=金矿，3=圣水瓶...）
   int level;          // 等级
   int gridX, gridY;   // 网格坐标（左下角）
-  enum State { CONSTRUCTING, BUILT } state; // 状态
+  enum State {
+    PLACING,        // 待放置状态
+    CONSTRUCTING,   // 建造中
+    BUILT           // 已建造完成
+  } state;
   long long finishTime; // 建造/升级完成时间戳（秒），已建好为0
 };
 
